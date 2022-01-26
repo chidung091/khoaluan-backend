0b613f44b75055a11fe489e92b28035a252961ff
Chi Dung <chidung091@gmail.com>
2022-01-26 18:00:00 +0700

    update

--- src/modules/time/dto/update-time.dto.ts
+++ src/modules/time/dto/update-time.dto.ts
@@ -1,59 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
-import { IsDate } from 'class-validator'
-import { IsEnum, IsNumber } from 'src/decorators/validators'
+import { IsEnum } from 'src/decorators/validators'
 import { Status } from '../time.enum'
 
 export class UpdateTimeDto {
   @ApiProperty()
-  @IsNumber({ notEmpty: false })
   startYear: number
 
   @ApiProperty()
-  @IsNumber({ notEmpty: false })
   endYear: number
 
   @ApiProperty()
-  @IsNumber({ notEmpty: false })
   semester: number
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   startTimeStudent: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   endTimeStudent: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   startTimeMonitor: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   endTimeMonitor: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   startTimeHeadMaster: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   endTimeHeadMaster: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   startTimeDepartment: Date
 
   @ApiProperty()
-  @IsDate()
   @Type(() => Date)
   endTimeDepartment: Date
 
